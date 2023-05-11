ce9762dc969090a6275d063c5e9c217f1b736b51
Michael <makinyemi@gmail.com>
2023-05-11 19:08:43 -0400

    Added Flask app config
    Set up configurations for app to access MySQL database from virtual
    environment variables.

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -5,3 +5,8 @@ from flask_mysqldb import MySQL
 app = Flask(__name__) 
 mysql = MySQL(app) 
 
+app.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+app.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
