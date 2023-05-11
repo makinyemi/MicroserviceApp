db02952aed7b06f154e573559632906af4d0fc9c
Michael <makinyemi@gmail.com>
2023-05-11 19:11:58 -0400

    Added /login endpoint to verify users
    Created a login endpoint that will use basic authentication to verify
    user to access to our application. From the request authorization header we grab the user name and password to verify against MySql databases Auth table. Taking email as username and a password.
    With valid credentials, the user is returned a JWT to access the application.

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -10,3 +10,25 @@ app.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
 app.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
 app.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+app.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "Authentication details not found", 401
+
+    cur = mysql.connection.cursor()
+    res = cur.execute("SELECT email, password FROM users WHERE email = %s", [auth.username])
+    cur.close()
+
+    if res > 0:
+        user = cur.fetchone()
+        email = user["email"]
+        password = user["password"]
+
+        if auth.username == email and auth.password == password:
+            return createJWT(email,app.config["SECRET_KEY"],True)
+        else:
+            return "Invalid credentials", 401
+    else:
+        return "User not found", 401
